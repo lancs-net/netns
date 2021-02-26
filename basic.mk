1793f28647237f8f39740d00483df06b50950d90
Alexander Jung <a.jung@lancs.ac.uk>
2021-02-26 15:04:37 +0000

    Bump version to v0.5.4
    Signed-off-by: Alexander Jung <a.jung@lancs.ac.uk>

--- basic.mk
+++ basic.mk
@@ -152,7 +152,7 @@ tag: ## Create a new git tag to prepare to build a release.
 	git tag -sa $(VERSION) -m "$(VERSION)"
 	@echo "Run git push origin $(VERSION) to push your new tag to GitHub and trigger a release."
 
-REGISTRY := r.j3ss.co
+REGISTRY := ghcr.io/lancs-net
 .PHONY: image
 image: ## Create the docker image from the Dockerfile.
 	@docker build --rm --force-rm -t $(REGISTRY)/$(NAME) .
